e2fa4dc069e9f3faab912eba83810e64cc8bcb5a
boopathypradeep <boopathypradeepr@gmail.com>
2022-04-29 19:44:33 +0530

    new upadates

---
+++ src/org/task1/git.java
@@ -0,0 +1,20 @@
+package org.task1;
+
+public class git {
+public static void main(String[] args) {
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+	System.out.println("hai");
+}
+}
